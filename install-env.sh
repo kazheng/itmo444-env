e74e9a86d1f67fb3699dbe475e2e338b650f6313
kazheng <kzheng8@hawk.iit.edu>
2015-11-23 22:40:57 -0600

    updated move paths

--- install-env.sh
+++ install-env.sh
@@ -5,8 +5,8 @@ sudo apt-get install -y apache2 git
 
 git clone https://github.com/kazheng/ITMO444.git
 
-cp ./ITMO444/images/* /var/www/html/images
-cp ./ITMO444/web/* /var/www/html
+cp ./ITMO444/images /var/www/html/images
+cp ./ITMO444/web/index.html /var/www/html
 
 echo "Hello World!" > /tmp/hello.txt
 
