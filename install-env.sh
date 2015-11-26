9b29701c0dab953c02c0d57650ae50265aeabfd1
kazheng <kzheng8@hawk.iit.edu>
2015-11-26 16:39:03 -0600

    blahj

--- install-env.sh
+++ install-env.sh
@@ -3,10 +3,10 @@
 sudo apt-get update -y
 sudo apt-get install -y apache2 git
 
-git clone https://github.com/kazheng/itmo444.git
+git clone https://github.com/kazheng/itmo444-app.git
 
-mv ./itmo444/images /var/www/html/images
-mv ./itmo444/web/index.html /var/www/html
+mv ./itmo444-app/images /var/www/html/images
+mv ./itmo444-app/index.html /var/www/html
 
 echo "Hello World!" > /tmp/hello.txt
 
