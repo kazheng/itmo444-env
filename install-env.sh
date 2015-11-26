9f87779079df06de722410530a19cdc36c2094e4
kazheng <kzheng8@hawk.iit.edu>
2015-11-26 16:11:10 -0600

    changed from cp to mv in env script

--- install-env.sh
+++ install-env.sh
@@ -3,10 +3,10 @@
 sudo apt-get update -y
 sudo apt-get install -y apache2 git
 
-git clone https://github.com/kazheng/ITMO444.git
+git clone https://github.com/kazheng/itmo444.git
 
-cp ./ITMO444/images /var/www/html/images
-cp ./ITMO444/web/index.html /var/www/html
+mv ./itmo444/images /var/www/html/images
+mv ./itmo444/web/index.html /var/www/html
 
 echo "Hello World!" > /tmp/hello.txt
 
